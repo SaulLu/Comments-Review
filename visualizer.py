2899bbd0505a74005dd00d6d5ced607a86ad10a8
SaulLu <lucilesaul.com@gmail.com>
2019-11-27 21:23:03 +0100

    word_cloud

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,27 @@
 import pandas as pandas
 import matplotlib.pyplot as plt
 import seaborn as sns
+from wordcloud import WordCloud, STOPWORDS
+stopwords = set(STOPWORDS)
+
+def show_wordcloud(data, title = None):
+    wordcloud = WordCloud(
+        background_color='white',
+        stopwords=stopwords,
+        max_words=200,
+        max_font_size=40, 
+        scale=3,
+        random_state=1 # chosen at random by flipping a coin; it was heads
+    ).generate(str(data))
+
+    fig = plt.figure(1, figsize=(12, 12))
+    plt.axis('off')
+    if title: 
+        fig.suptitle(title, fontsize=20)
+        fig.subplots_adjust(top=2.3)
+
+    plt.imshow(wordcloud)
+    plt.show()
 
 def count_number_sondage(dataframe):
     return(dataframe[["Verbatim", "Nom du sondage"]].groupby("Nom du sondage").agg("count").rename(columns={
